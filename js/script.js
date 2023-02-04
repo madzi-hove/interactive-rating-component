49276db5654ab6820f10160020182bdbc3ac5b77
Madzi Hove <madzihove@gmail.com>
2023-02-04 10:57:30 +0200

    add form validation

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("[type='radio']");
+let notChecked = true;
+console.log(inputs);
+
+// Removes active state class
+function removeActiveState() {
+	inputs.forEach((input) => {
+		if (input.parentNode.classList.contains("rating--checked")) {
+			input.parentNode.classList.remove("rating--checked");
+		}
+	});
+}
+
+// Checks for active radio button
+function checkForInput(inputs) {
+	return inputs.every((input) => {
+		return input.checked === false;
+	});
+}
+
+// Add event listener to form and use bubbling to caputre event
+form.addEventListener("click", (e) => {
+	if (e.target.type === "radio") {
+		removeActiveState();
+		e.target.parentNode.classList.add("rating--checked");
+	}
+
+	if (e.target.type === "submit") {
+		const inputsArray = [...inputs];
+		notChecked = checkForInput(inputsArray);
+		if (notChecked === true) e.preventDefault();
+	}
+});
+
+form.addEventListener("submit", () => {
+	console.log("submitted");
+});
