478bd289aa56a58b2b72629105eb0a7d527cb050
Madzi Hove <madzihove@gmail.com>
2023-02-07 10:55:14 +0200

    remove uneccessary code

--- js/script.js
+++ js/script.js
@@ -1,65 +1,19 @@
 const form = document.querySelector("form");
-const inputs = document.querySelectorAll("[type='radio']");
 const ratingContainer = document.querySelector(".form__ratings");
 const ratingSection = document.querySelector(".rating");
 const thankYouSection = document.querySelector(".thank-you");
 const rating = document.querySelector(".rating-value");
-let notChecked = true;
-let formdata;
-console.log(inputs);
 
-// Removes active state class
-function removeActiveState() {
-	inputs.forEach((input) => {
-		if (input.parentNode.classList.contains("rating--checked")) {
-			input.parentNode.classList.remove("rating--checked");
-		}
-	});
-}
+// Add event listener to form and use bubbling to caputre event
+form.addEventListener("click", (e) => {
+	if (e.target.type === "radio") {
+		e.target.parentNode.classList.add("rating--checked");
+	}
+});
 
-// Checks for active radio button
-function checkForInput(inputs) {
-	return inputs.every((input) => {
-		return input.checked === false;
-	});
-}
-
-// Add class to radio button container to show message
-function invalidMessage() {
-	ratingContainer.classList.add("invalid");
-	setTimeout(() => {
-		ratingContainer.classList.remove("invalid");
-	}, 1000);
-}
-
-window.addEventListener("load", () => {
-	const inputsArray = [...inputs];
-	inputsArray.forEach((input) => {
-		input.checked = false;
-	});
-
-	// Add event listener to form and use bubbling to caputre event
-	form.addEventListener("click", (e) => {
-		if (e.target.type === "radio") {
-			removeActiveState();
-			e.target.parentNode.classList.add("rating--checked");
-		}
-
-		if (e.target.type === "submit") {
-			notChecked = checkForInput(inputsArray);
-			if (notChecked === true) {
-				e.preventDefault();
-				invalidMessage();
-			}
-		}
-	});
-
-	form.addEventListener("submit", (e) => {
-		ratingSection.classList.add("rating--hidden");
-		thankYouSection.classList.remove("thank-you--hidden");
-		formData = new FormData(form);
-		rating.innerText = formData.get("rating");
-		e.preventDefault();
-		console.log(Number(formData.get("rating")));
-	});
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	ratingSection.classList.add("rating--hidden");
+	thankYouSection.classList.remove("thank-you--hidden");
+	rating.innerText = form.rating.value;
 });
